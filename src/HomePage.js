fe6fe3e83a75b4914970818b4134a2e0209527b7
Judy Ghashim <judyghashim@Judys-MacBook-Pro.local>
2020-05-02 12:22:13 -0500

    finishing the homepage

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,3 +1,19 @@
-import React from 'react'
-import {useHistory} from "react-router-dom"
+import React from "react";
+import { useHistory } from "react-router-dom";
 
+const HomePage = () => {
+  const history = useHistory();
+  const pizzaPage = (e) => {
+    history.push("/pizza");
+  };
+  return (
+    <div className="homepage-wrapper">
+      <h2>Order Pizza</h2>
+      <p>choose you own pizza</p>
+      <button className="pizza-button" onClick={pizzsPage}>
+        pizza
+      </button>
+    </div>
+  );
+};
+export default HomePage;
