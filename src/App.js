430a1469ced9eebe39760784edc8890e48447c98
Judy Ghashim <judyghashim@Judys-MacBook-Pro.local>
2020-05-02 13:13:05 -0500

    start working on styling

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React from "react";
-import HomePage from "./HomePage";
+import HomePage from './HomePage';
 import Pizza from "./Pizza";
 import { Route, Link, Switch } from 'react-router-dom';
-import "./App.css";
+import "./App.css"
 
 const App = () => {
   return (
     <div className="App">
-      <nav>
-        <h1 className="leHeader">Lambda Eats</h1>
-        <div className="nav-links">
-          <Link to="/">
-            <button className="homelink">Home</button>
-          </Link>
-          <Link to="/pizza">
-            <button className="homelink">Menu</button>
-          </Link>
-        </div>
-      </nav>
-      <Switch>
-        <Route path="/pizza" component={Pizza} />
+    <nav>
+      <h1 className="leHeader">Lambda Eats</h1>
+      <div className="nav-links">
+        <Link  to="/">
+          <button className="homelink">Home</button></Link>
+        <Link to="/pizza">
+           <button className="homelink">Menu</button></Link>
+      </div>
+     </nav>
+     <Switch>
+       <Route path="/pizza" component={Pizza} />
 
-        <Route exact path="/" component={HomePage} />
-      </Switch>
-    </div>
+       <Route exact path="/" component={HomePage} />
+     </Switch>
+     </div>
   );
 };
 export default App;
